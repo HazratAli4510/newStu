9ce24f426aa906df80db664e01963ed896ed5ddb
Md Hazrat Ali <md.hazratali4510@gmail.com>
2022-03-20 19:11:58 +0600

    changing

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,6 @@ function Home({ data }) {
     <>
       <Head>
         <title>Work</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossOrigin="anonymous" />
       </Head>
       <body>
         <div className="container">
@@ -143,7 +142,7 @@ export async function getStaticProps() {
     props: {
       data
     },
-    revalidate: 10
+    revalidate: 1
   }
 }
 
